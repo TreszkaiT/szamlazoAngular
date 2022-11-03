c37090518cedff7fabf67a94d5a054db3a8c8a8d
TreszkaiT <treszkaitom@gmail.com>
2022-11-03 14:37:08 +0100

    routing makes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -9,20 +9,32 @@ import { FormsModule } from '@angular/forms';
 import { LeftSidebarComponent } from './left-sidebar/left-sidebar.component';
 import { ContentComponent } from './content/content.component';
 import { TopHeaderComponent } from './top-header/top-header.component';
+import { RouterModule, Routes } from '@angular/router';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { CurrencyComponent } from './currency/currency.component';
+
+const roterSettings: Routes = [
+  { path: '', component: ContentComponent},
+  { path: "currency", component: CurrencyComponent},
+  { path: '**', component: PageNotFoundComponent}
+]
 
 @NgModule({
   declarations: [
     AppComponent,
     LeftSidebarComponent,
     ContentComponent,
-    TopHeaderComponent
+    TopHeaderComponent,
+    PageNotFoundComponent,
+    CurrencyComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FontAwesomeModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(roterSettings)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/model/currency.ts
@@ -0,0 +1,19 @@
+export class Currency {
+    id: number
+    nameShort: string
+    nameLong: string
+
+    constructor(id: number, nameShort: string, nameLong: string){
+        this.id = id
+        this.nameShort = nameShort
+        this.nameLong = nameLong
+    }
+
+    fromObject(obj: any){
+        for(let i in obj){
+            this.id = obj.id
+            this.nameShort = obj.nameShort
+            this.nameLong = obj.nameLong
+        }
+    }
+}
